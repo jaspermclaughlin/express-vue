d459bce27b7f1c65ce6818d415951730bc8d6fce
Jasper McLaughlin <jasper-mclaughlin@outlook.de>
2024-04-03 13:28:01 +0200

    todo POC implemented

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/*', (req, res) => {
+app.use(express.json())
+
+app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+const todos = []
+
+app.get('/api/todo', (req, res) => {
+  res.send(todos)
+})
+
+app.post('/api/todo', (req, res) => {
+  const todo = req.body.todo
+  todos.push(todo)
+  res.status(201)
+  res.send()
 })
 
 app.listen(port, () => {
