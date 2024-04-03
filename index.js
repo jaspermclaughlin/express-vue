c0d4e7da0cc6ec379f6d08ce9f46a42773062e98
Jasper McLaughlin <jasper-mclaughlin@outlook.de>
2024-04-03 15:25:20 +0200

    Added prettier

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-const express = require('express')
-const db = require('./db');
-const app = express()
-const port = 3000
+const express = require("express");
+const db = require("./db");
+const app = express();
+const port = 3000;
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/api/todo', (req, res) => {
+app.get("/api/todo", (req, res) => {
   const response = db.query(`SELECT * FROM todos`, []);
-  res.send(response)
-})
+  res.send(response);
+});
 
-app.post('/api/todo', (req, res) => {
-  const todo = req.body.todo
-  db.run(`INSERT INTO TODOS(todo,author) values(? , ?)`, [todo.content, todo.author])
-  res.status(201)
-  res.send()
-})
+app.post("/api/todo", (req, res) => {
+  const todo = req.body.todo;
+  db.run(`INSERT INTO TODOS(todo,author) values(? , ?)`, [
+    todo.content,
+    todo.author,
+  ]);
+  res.status(201);
+  res.send();
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
